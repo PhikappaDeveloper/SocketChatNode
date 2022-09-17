2d231c57b2e79338dd1d87654156e68c600bdf99
Fernando Carrillo <fer.lfcb9@gmail.com>
2022-09-16 19:33:51 -0500

    Actualizacion Version 2

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -20,15 +20,18 @@ io.on('connection', (client) => {
     let personas = usuarios.agregarPersona(client.id, data.nombre, data.sala)
 
     client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonaPorSala(data.sala))
-
+    client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } Se unio`))
     callback(usuarios.getPersonaPorSala(data.sala))
 
   })
 
-  client.on('crearMensaje',(data) => {
+  client.on('crearMensaje',(data, callback) => {
     let persona = usuarios.getPersona(client.id)
     let mensaje = crearMensaje(data.nombre, data.mensaje)
     client.broadcast.to(persona.sala).emit('crearMensaje', mensaje)
+    
+    callback(mensaje)
+   
   })
 
   client.on('disconnect', () => {
@@ -37,6 +40,8 @@ io.on('connection', (client) => {
 
     client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${ personaBorrada.nombre } salio del chat`))
     client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonaPorSala(personaBorrada.sala))
+
+    callback(mensaje)
 
   }) 
 
